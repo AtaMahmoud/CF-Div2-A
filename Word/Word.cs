577ac8dd14e2bcd05f82e2292c73299e27c10894
Ata                                       Mahmoud <ata.ata250@hotmail.com>
2017-08-16 03:17:45 +0200

    Create Word.cs

---
+++ Word/Word.cs
@@ -0,0 +1,24 @@
+using System;
+class program
+{
+    static void Main(string[]args)
+    {
+        string word = Console.ReadLine();
+            
+            int upperCaseCounter = 0;
+            int lowerCaseCounter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] >= 'a')
+                    lowerCaseCounter++;
+                else if (word[i] >= 'A')
+                    upperCaseCounter++;
+            }
+            if(lowerCaseCounter>upperCaseCounter)
+                Console.WriteLine(word.ToLower());
+            else if(lowerCaseCounter<upperCaseCounter)
+                Console.WriteLine(word.ToUpper());
+            else
+                Console.WriteLine(word.ToLower());
+    }
+}
